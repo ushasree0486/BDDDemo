0af05b0ba96fb0c1033c36874235cf491ddd91cc
Usha J <ushasree.jpl@gmail,.com>
2024-08-19 23:25:11 +0530

    initial commit

--- src/test/java/api/cucumber/steps/PostSteps.java
+++ src/test/java/api/cucumber/steps/PostSteps.java
@@ -1,11 +1,12 @@
 package api.cucumber.steps;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class PostSteps {
-    @Given("User should be logged in and should be present on his wall")
+   /* @Given("User should be logged in and should be present on his wall")
     public void user_should_be_logged_in_and_should_be_present_on_his_wall() {
         // Code to ensure user is logged in and on their wall
 
@@ -31,4 +32,24 @@ public class PostSteps {
         // Code to verify the message has been posted
         System.out.println("Message posted successfully");
     }
+
+    @When("User supply the youtube link in the text box")
+    public void userSupplyTheYoutubeLinkInTheTextBox() {
+    }
+
+    @Then("Then video should get posted on the user wall")
+    public void thenVideoShouldGetPostedOnTheUserWall() {
+    }
+
+    @And("the video should have proper thumbnail")
+    public void theVideoShouldHaveProperThumbnail() {
+    }
+
+    @When("I type the message as {string} in the text box")
+    public void iTypeTheMessageAsInTheTextBox(String arg0) {
+    }
+
+    @When("User supply the youtube link as {string} in the text box")
+    public void userSupplyTheYoutubeLinkAsInTheTextBox(String arg0) {
+    }*/
 }
