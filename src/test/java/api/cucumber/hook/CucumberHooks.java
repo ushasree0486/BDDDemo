5edc447be720e796b3f094a81462868e51d4530b
Usha J <ushasree.jpl@gmail,.com>
2024-08-20 22:42:09 +0530

    initial commit

---
+++ src/test/java/api/cucumber/hook/CucumberHooks.java
@@ -0,0 +1,15 @@
+package api.cucumber.hook;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class CucumberHooks {
+    @Before
+    public void setUp(){
+        System.out.println("---Before hook---");
+    }
+    @After
+    public void tearDown(){
+        System.out.println("---After hook---");
+    }
+}
