8ef8d1c72b47f4358ee3ee768f2be38dfd6d8314
Usha J <ushasree.jpl@gmail,.com>
2024-08-20 19:51:01 +0530

    initial commit

--- src/test/java/api/cucumber/steps/LoginStepDefinitionforScenarioWithExamples.java
+++ src/test/java/api/cucumber/steps/LoginStepDefinitionforScenarioWithExamples.java
@@ -4,22 +4,39 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LoginStepDefinitionforScenarioWithExamples {
+    private WebDriver webDriver;
+
 
     @Given("Use is on Home page")
     public void useIsOnHomePage() {
+        WebDriverManager.chromedriver().setup();
+        //WebDriver driver = new FirefoxDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        webDriver = new ChromeDriver(options);
+        webDriver.get("https://www.saucedemo.com/");
+
     }
 
     @When("User Navigate to Login Page")
     public void userNavigateToLoginPage() {
+        System.out.println("User Navigate to Login Page");
     }
-
-    @And("User enters {string} and {string}")
-    public void userEntersAnd(String arg0, String arg1, String arg2, String arg3) {
-    }
+    
 
     @Then("Message displayed Login Successfully")
     public void messageDisplayedLoginSuccessfully() {
+    }
+
+    @When("User enters {string} and {string}")
+    public void userEntersAnd("User name is " +username + "password is " + password) {
+
     }
 }
