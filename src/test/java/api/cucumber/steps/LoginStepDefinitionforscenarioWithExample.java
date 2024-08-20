24fc8a59314e782875798563b0dbcabb26c03a1c
Usha J <ushasree.jpl@gmail,.com>
2024-08-20 18:45:34 +0530

    initial commit

---
+++ src/test/java/api/cucumber/steps/LoginStepDefinitionforscenarioWithExample.java
@@ -0,0 +1,24 @@
+package api.cucumber.steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinitionforscenarioWithExample {
+    @Given("Use is on Home page")
+    public void useIsOnHomePage() {
+    }
+
+    @When("User Navigate to Login Page")
+    public void userNavigateToLoginPage() {
+    }
+
+    @And("User enters {string} and {string}")
+    public void userEntersAnd(String arg0, String arg1, String arg2, String arg3) {
+    }
+
+    @Then("Message displayed Login Successfully")
+    public void messageDisplayedLoginSuccessfully() {
+    }
+}
