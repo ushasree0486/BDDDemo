bc615ae4981103f4abfe537a99fd0dad0a38c203
Usha J <ushasree.jpl@gmail,.com>
2024-08-21 22:19:46 +0530

    initial commit

--- src/test/java/api/cucumber/Pages/LoginPage.java
+++ src/test/java/api/cucumber/Pages/LoginPage.java
@@ -5,14 +5,20 @@ import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
     private WebDriver webDriver;
-    private By txt_username=By.id("user-name");
+   /* private By txt_username=By.id("user-name");
     private By txt_password=By.id("password");
     private By btn_login=By.id("login-button");
     private By ltn_logout=By.id("logout_sidebar_link");
+*/
+    private By  txt_username= By.name("email");
+    private By txt_password = By.name("password");
+    private By btn_login = By.xpath("//input[@type='submit']");
+    //private By forgottenPasswordLinkLocator = By.linkText("Forgotten Password");
+    private By btn_logout = By.linkText("Logout");
 
     public LoginPage(WebDriver webDriver){
         this.webDriver=webDriver;
-        if(!webDriver.getTitle().equals("saucedemo")){
+        if(!webDriver.getTitle().equals("Naveen automation labs")){
             throw new IllegalStateException("This is not Login Page.The current page is "+webDriver.getCurrentUrl());
         }
     }
