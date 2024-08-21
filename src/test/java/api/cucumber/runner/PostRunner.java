11041b03d5fd61b3d06ab63b6ea0b3feecf015ce
Usha J <ushasree.jpl@gmail,.com>
2024-08-21 18:47:48 +0530

    initial commit

--- src/test/java/api/cucumber/runner/PostRunner.java
+++ src/test/java/api/cucumber/runner/PostRunner.java
@@ -5,17 +5,18 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-/*
-@CucumberOptions(features = {"src/test/java/api/cucumber/features"},
+
+@CucumberOptions(features = {"src/test/java/api/cucumber/features/login_demo.feature"},
         glue = {"api/cucumber/steps"}
         , dryRun = false)
-*/
-@CucumberOptions(features = {"src/test/java/api/cucumber/features/posts.feature"},
+/*
+@CucumberOptions(features = {"src/test/java/api/cucumber/features/login_demo.feature"},
         glue = {"api/cucumber/steps", "api/cucumber/hook"}
         //,plugin={"pretty","html:target/HtmlReports"}
         //,plugin={"pretty","json:target/report.json"}
         ,plugin={"pretty","junit:target/report.xml"},tags="@SmokeTest"
 )
+*/
 
 public class PostRunner {
 
