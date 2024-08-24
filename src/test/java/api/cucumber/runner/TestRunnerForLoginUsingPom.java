41cca3fa8e3e1f6cb40a44212194c92232604353
Usha J <ushasree.jpl@gmail,.com>
2024-08-24 23:53:54 +0530

    initial commit

---
+++ src/test/java/api/cucumber/runner/TestRunnerForLoginUsingPom.java
@@ -0,0 +1,12 @@
+package api.cucumber.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+public class TestRunnerForLoginUsingPom {
+    @RunWith(Cucumber.class)
+
+    CucumberOptions(features=)
+
+}
