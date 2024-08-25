0a92a9013bcb253452f71aa347659dd3f2e261c0
Usha J <ushasree.jpl@gmail,.com>
2024-08-25 21:52:18 +0530

    initial commit

--- src/test/java/api/cucumber/runner/TestRunner_RunWithSingleTag.java
+++ src/test/java/api/cucumber/runner/TestRunner_RunWithSingleTag.java
@@ -5,10 +5,10 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/java/api/cucumber/featurwithtags"},
+@CucumberOptions(features = {"src/test/java/api/cucumber/featurewithtags"},
         glue = {"api/cucumber/steps"},
-        //tags = "@smoke",  //deprecate
-        tags = "@important or @regression"
+        tags = "@smoke" //deprecate
+        //tags = "@important or @regression"
         //tags = {"@smoke and @regression"}
 )
 public class TestRunner_RunWithSingleTag {
