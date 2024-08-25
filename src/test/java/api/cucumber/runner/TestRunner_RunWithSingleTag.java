75bd055a23ec4023fe76f16789ffb01cfb845687
Usha J <ushasree.jpl@gmail,.com>
2024-08-25 20:50:42 +0530

    initial commit

---
+++ src/test/java/api/cucumber/runner/TestRunner_RunWithSingleTag.java
@@ -0,0 +1,15 @@
+package api.cucumber.runner;
+
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features = {"src/test/java/api/cucumber/features/featurewithtags"},
+        glue = {"api/cucumber/steps"})
+
+
+public class TestRunner_RunWithSingleTag {
+
+}
